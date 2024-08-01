c0a98ef49291e2f29b4bd7201fea11235fe84956
ymc0182 <501414437@qq.com>
2024-08-01 22:11:00 +0800

    update

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '976px',
+			xl: '1080px',
+		  },
+	},
+	plugins: [],
+}
